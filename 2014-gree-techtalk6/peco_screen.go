5e45b87384271f7f31ba05c76e59c818471c8c6d
Daisuke Maki <lestrrat+github@gmail.com>
2014-09-23 10:14:09 +0900

    90% done, methinks

---
+++ 2014-gree-techtalk6/peco_screen.go
@@ -0,0 +1,50 @@
+// MODIFIED FROM ORIGINAL peco CODE TO FIT PRESENTATION
+
+package peco
+
+import "github.com/nsf/termbox-go"
+
+// START SCREEN OMIT
+// Screen hides termbox from tne consuming code so that
+// it can be swapped out for testing
+type Screen interface {
+	Clear(termbox.Attribute, termbox.Attribute) error
+	Flush() error
+	PollEvent() chan termbox.Event
+	SetCell(int, int, rune, termbox.Attribute, termbox.Attribute)
+	Size() (int, int)
+}
+// END SCREEN OMIT
+
+// Termbox just hands out the processing to the termbox library
+type Termbox struct{}
+
+// START TERMBOX OMIT
+func (t Termbox) Clear(fg, bg termbox.Attribute) error {
+	return termbox.Clear(fg, bg)
+}
+
+func (t Termbox) Flush() error {
+	return termbox.Flush()
+}
+
+func (t Termbox) SetCell(x, y int, ch rune, fg, bg termbox.Attribute) {
+	termbox.SetCell(x, y, ch, fg, bg)
+}
+
+func (t Termbox) Size() (int, int) {
+	return termbox.Size()
+}
+
+func (t Termbox) PollEvent() chan termbox.Event {
+	evCh := make(chan termbox.Event)
+	go func() {
+		defer func() { recover() }()
+		defer func() { close(evCh) }()
+		for {
+			evCh <- termbox.PollEvent()
+		}
+	}()
+	return evCh
+}
+// END TERMBOX OMIT
